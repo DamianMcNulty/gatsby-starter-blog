53296bf2811523fe4a19fdeccbe437fa7ee7da66
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-26 09:11:51 -0400

    add a screenshot

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,8 @@
+/// <reference types="cypress" />
 beforeEach(() => {
   cy.visit('/')
 })
 it('works', () => {
   cy.contains('Combination of Gatsby to Netlify and tested using Cypress.io is pretty cool.')
+  cy.screenshot('site')
 })
