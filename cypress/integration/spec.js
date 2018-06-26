272d4b14656af6421644fe21c915a6413aeeefa7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-26 09:14:43 -0400

    capture screenshot of the entire runner

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -4,5 +4,5 @@ beforeEach(() => {
 })
 it('works', () => {
   cy.contains('Combination of Gatsby to Netlify and tested using Cypress.io is pretty cool.')
-  cy.screenshot('site')
+  cy.screenshot('site', {capture: 'runner'})
 })
